a83f6a2ac725d444b410689c03699cdae3aeed92
rahulroyyy6-cmyk <rahul.royyy6@gmail.com>
2026-04-21 23:16:34 +0530

    made headless mode for github action run

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ const config = ({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 browserName: 'firefox',
-headless : false
+headless : true
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // trace: 'on-first-retry',
   },
